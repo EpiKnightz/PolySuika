8a82a4d3f8a6d48709793434e2365283673c24fa
EpiKnightz <EpiKnightz@gmail.com>
2026-02-03 15:44:26 +0700

    - Add support for multiple events listening

--- PolySuika/Assets/Scripts/Managers/InputManager.cs
+++ PolySuika/Assets/Scripts/Managers/InputManager.cs
@@ -7,25 +7,62 @@ public class InputManager : MonoBehaviour
 {
     public LeanFingerTap LeanTapScript;
 
-    [BetterHeader("Listen To")]
-    public VoidEventChannelSO ECActionHidden;
-    public VoidEventChannelSO ECOnActionAnimFinished;
+    [BetterHeader("Broadcast On")]
+    public VectorEventChannelSO ECOnClickPosition;
 
-    private void Start()
-    {
-        OnEnableGameplay(false);
-    }
+    [Header("Listen To")]
+    public VoidEventChannelSO ECOnPauseInput;
+    public VoidEventChannelSO ECOnUnpauseInput;
+    public VoidEventChannelSO[] ECOnNeedDisableInput;
+    public VoidEventChannelSO[] ECOnNeedEnableInput;
+
+    // Privates
+    private bool IsPause = true;
 
     private void OnEnable()
     {
-        ECOnActionAnimFinished.Sub(OnActionPhaseStart);
-        ECActionHidden.Sub(OnActionPhaseEnd);
+        for (int i = 0; i < ECOnNeedEnableInput.Length; i++)
+        {
+            ECOnNeedEnableInput[i].Sub(OnActionPhaseStart);
+        }
+        for (int i = 0; i < ECOnNeedDisableInput.Length; i++)
+        {
+            ECOnNeedDisableInput[i].Sub(OnActionPhaseEnd);
+        }
+        ECOnPauseInput.Sub(OnPause);
+        ECOnUnpauseInput.Sub(Unpause);
     }
 
     private void OnDisable()
     {
-        ECOnActionAnimFinished.Unsub(OnActionPhaseStart);
-        ECActionHidden.Unsub(OnActionPhaseEnd);
+        for (int i = 0; i < ECOnNeedEnableInput.Length; i++)
+        {
+            ECOnNeedEnableInput[i].Unsub(OnActionPhaseStart);
+        }
+        for (int i = 0; i < ECOnNeedDisableInput.Length; i++)
+        {
+            ECOnNeedDisableInput[i].Unsub(OnActionPhaseEnd);
+        }
+        ECOnPauseInput.Unsub(OnPause);
+        ECOnUnpauseInput.Unsub(Unpause);
+    }
+
+    public void OnPause()
+    {
+        IsPause = true;
+    }
+
+    public void Unpause()
+    {
+        IsPause = false;
+    }
+
+    public void OnClick(Vector3 position)
+    {
+        if (!IsPause)
+        {
+            ECOnClickPosition.Invoke(position);
+        }
     }
 
     private void OnEnableGameplay(bool bIsEnable)
