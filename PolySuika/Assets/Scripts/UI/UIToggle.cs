03c5642ed41fdf5054618698d23f833d8644968a
EpiKnightz <EpiKnightz@gmail.com>
2026-02-03 15:18:04 +0700

    - Require component and tweak UI toggle for more general functions

--- PolySuika/Assets/Scripts/UI/UIToggle.cs
+++ PolySuika/Assets/Scripts/UI/UIToggle.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-
+[RequireComponent(typeof(Image))]
 public class UIToggle : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Sprite EnableToggle;
+    [SerializeField] private Sprite DisableToggle;
+    [SerializeField] private Image TargetImage;
+
+    // Privates
     protected bool UIStateEnable = true;
 
     public void OnClick()
@@ -20,6 +27,32 @@ public class UIToggle : MonoBehaviour
     }
 
     public virtual void ToggleAction(bool newState) { }
-    public virtual void EnableVisual() { }
-    public virtual void DisableVisual() { }
+    public virtual void DisableVisual()
+    {
+        if (TargetImage != null
+            && DisableToggle != null)
+        {
+            TargetImage.sprite = DisableToggle;
+        }
+    }
+
+    public virtual void EnableVisual()
+    {
+        if (TargetImage != null
+            && EnableToggle != null)
+        {
+            TargetImage.sprite = EnableToggle;
+        }
+    }
+
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (TargetImage == null)
+        {
+            TargetImage = GetComponent<Image>();
+        }
+    }
+#endif
 }
