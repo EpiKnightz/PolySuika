6bed48db31f5f35b31a7ae26740155fe23a03046
EpiKnightz <EpiKnightz@gmail.com>
2026-02-03 18:27:33 +0700

    - Disable back key code since Unity input system still refuse to route back key to the function

--- PolySuika/Assets/Scripts/UI/UIBackHandler.cs
+++ PolySuika/Assets/Scripts/UI/UIBackHandler.cs
@@ -3,27 +3,28 @@ using UnityEngine;
 
 public class UIBackHandler : MonoBehaviour
 {
-    [Inject] private readonly IUIManager UIManager;
+    // Back key currently didn't work on Android, so no need to inject the dependency here
+    // Still, in perfect world, back key would go back to menu, then quit the app
+    //[Inject] private readonly IUIManager UIManager;
 
     public void OnClick()
     {
-        if (UIManager != null
-            && UIManager.GetCurrentActivePanel() != PanelType.Menu)
+        // if (UIManager != null
+        //     && UIManager.GetCurrentActivePanel() != PanelType.Menu)
+        // {
+        //     UIManager.SwitchActivePanel(PanelType.Menu, true);
+        // }
+        // else
+        // {
+        if (Application.platform == RuntimePlatform.Android)
         {
-            UIManager.SwitchActivePanel(PanelType.Menu, true);
+            AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+            activity.Call<bool>("moveTaskToBack", true);
         }
         else
         {
-            if (Application.platform == RuntimePlatform.Android)
-            {
-                Debug.Log("Back button clicked");
-                AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
-                activity.Call<bool>("moveTaskToBack", true);
-            }
-            else
-            {
-                Application.Quit();
-            }
+            Application.Quit();
         }
+        //}
     }
 }
